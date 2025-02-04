5586bf8ce50f3de7d0810ac648372f96785aa5ee
Vinicius Cândido <v.oliveiracandido@yahoo.com>
2025-02-04 16:51:35 -0300

    Add blur
    Adicionado blur na página ao abrir o menu hamburguer

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+
+
 function menuShow() {
     let menuMobile = document.querySelector(".menu-mobile");
+    let overlay = document.querySelector(".overlay");
+    
     if (menuMobile.classList.contains("open")){
         menuMobile.classList.remove("open");
         document.querySelector(".icon").src = "img/menu-hamburguer.svg"
+        overlay.style.display = "none";
+
+        
     } else {
         menuMobile.classList.add("open");
         document.querySelector(".icon").src = "img/close-menu.svg"
-
+        overlay.style.display = "block"
     }
 
 }
