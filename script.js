2ba7ebba1a65eb038db286cb6a6fae197479d0d6
Vinicius Cândido <v.oliveiracandido@yahoo.com>
2025-02-05 11:56:57 -0300

    Add Scroll Reveal

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-
-
 function menuShow() {
     let menuMobile = document.querySelector(".menu-mobile");
     let overlay = document.querySelector(".overlay");
-    
-    if (menuMobile.classList.contains("open")){
+
+    if (menuMobile.classList.contains("open")) {
         menuMobile.classList.remove("open");
         document.querySelector(".icon").src = "img/menu-hamburguer.svg"
         overlay.style.display = "none";
 
-        
+
     } else {
         menuMobile.classList.add("open");
         document.querySelector(".icon").src = "img/close-menu.svg"
@@ -17,3 +15,34 @@ function menuShow() {
     }
 
 }
+
+ScrollReveal().reveal(".cta", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%"
+});
+ScrollReveal().reveal(".titulo", {
+    origin: "left",
+    duration: 1000,
+    distance: "20%"
+});
+ScrollReveal().reveal(".titulo", {
+    origin: "top",
+    duration: 1000,
+    distance: "20%"
+});
+ScrollReveal().reveal(".paragrafoTecnologia", {
+    origin: "right",
+    duration: 2000,
+    distance: "20%"
+});
+ScrollReveal().reveal(".imagemSectionTecnologia", {
+    origin: "left",
+    duration: 1000,
+    distance: "20%"
+});
+ScrollReveal().reveal(".fa", {
+    origin: "left",
+    duration: 2000,
+    distance: "30%"
+});
